c8e6bd169f8adb3f12382624ef4a12b1c34fdb2b
muradav <muradav@code.edu.az>
2022-03-14 03:31:43 -0700

    feat: Task4'un overloading method'u ile yeniden ishlenmesi

---
+++ Task4.Area.Overloading.Method/Task4.Area.Overloading.Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task4.Area.Overloading.Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreaMethod(5));
+        }
+
+        static double AreaMethod(int radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        static int AreaMethod(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double AreaMethod(int a, int b, int c, double r)
+        {
+            return (a + b + c) / 2 * r;
+        }
+
+
+    }
+}
